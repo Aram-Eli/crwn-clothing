af35f4a3f5a67553f9d73e8d27bdf2b17cfca7d5
Aram Eli <Aram221b@gmail.com>
2022-06-15 22:35:28 +0100

    created checkout page along with checkout items  and checkout cart functionality

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -10,7 +10,7 @@ const CartItem = ({cartItem}) => {
         <img src={imageUrl} alt={`${name}`}/>
         <div className="item-details">
           <span className='name'>{name}</span>
-          <span className='price'>{quantity} X ${price}</span>
+          <span className='price'>{quantity }  X  £{ price }</span>
         </div>
      </div>
   );
