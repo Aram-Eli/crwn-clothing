3a785225faa78a439b36575bf9f21e3971491340
Aram Eli <Aram221b@gmail.com>
2022-06-22 20:55:21 +0100

    Converted All of my component files from SCSS to JSX styled-components as there was alot of clashes within the page or name somehow I confused so with styled compoent its still styles but with JS syntex and show classNames different on the DOM which i thought its pretty cool

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -1,93 +1,86 @@
 
 
-import {useState} from "react";
+import { useState } from 'react';
 
 import FormInput from '../form-input/form-input.component';
-import Button from '../button/button.component';
+import Button, { BUTTON_TYPE_CLASSES } from '../button/button.component';
 
+import {
+  signInAuthUserWithEmailAndPassword,
+  signInWithGooglePopup,
+} from '../../utils/firebase/firebase.utils';
 
-import {signInWithGooglePopup,  
-        createUserDocumentFromAuth, 
-        signInAuthUserWithEmailAndPassword} 
-from "../../utils/firebase/firebase.utils";
-
-import './sign-in-form.styles.scss';
+import { SignInContainer, ButtonsContainer } from './sign-in-form.styles';
 
 const defaultFormFields = {
-    email: '',
-    password: '',
-} 
+  email: '',
+  password: '',
+};
 
 const SignInForm = () => {
-    const [formFields, setFormFields] = useState(defaultFormFields);
-    const {email, password} = formFields;
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
 
-    const resetFormFields = () => {
-      setFormFields(defaultFormFields);
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    await signInWithGooglePopup();
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      await signInAuthUserWithEmailAndPassword(email, password);
+      resetFormFields();
+    } catch (error) {
+      console.log('user sign in failed', error);
     }
+  };
 
-    const signInWithGoogle = async () => {
-      await signInWithGooglePopup();
-   };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
 
+    setFormFields({ ...formFields, [name]: value });
+  };
 
-    const handleSubmit = async (event) => {
-      event.preventDefault();
-      
-      try {
-        const {user} = await signInAuthUserWithEmailAndPassword(email, password);
-        resetFormFields();
-      } catch (error) {
-        switch(error.code) {
-           case 'auth/wrong-password':
-            alert('incorrect password for email');
-            break;
-           case 'auth/user-not-found':
-            alert('No user associcated with this email');
-            break;
-          default:
-            console.log(error)
-        }
-        console.log(error);
-      }
-    };
-
-    const handleChange = (event) => {
-       const {name, value} = event.target;
-
-       setFormFields({ ...formFields, [name]: value });
-    };
-
-    return(
-      <div className="sign-up-container">
-        <h2>Already have an account?</h2>
-        <span>Sign Up with your Email and Password</span>
-        <form onSubmit={handleSubmit}>
-          <FormInput 
+  return (
+    <SignInContainer>
+      <h2>Already have an account?</h2>
+      <span>Sign in with your email and password</span>
+      <form onSubmit={handleSubmit}>
+        <FormInput
           label='Email'
-          type="email" 
-          required 
-          onChange={handleChange} 
-          name="email" 
+          type='email'
+          required
+          onChange={handleChange}
+          name='email'
           value={email}
-          />
+        />
 
-          <FormInput 
+        <FormInput
           label='Password'
-          type="password" 
-          required 
-          onChange={handleChange} 
-          name="password" 
+          type='password'
+          required
+          onChange={handleChange}
+          name='password'
           value={password}
-          />
-
-          <div className='button-container'>
-            <Button type="submit">Sign In</Button>
-            <Button type='button' buttontype='google' onClick={signInWithGoogle}>Google</Button>
-          </div>
-        </form>
-      </div>
-    );
-}
+        />
+        <ButtonsContainer>
+          <Button type='submit'>Sign In</Button>
+          <Button
+            buttonType={BUTTON_TYPE_CLASSES.google}
+            type='button'
+            onClick={signInWithGoogle}
+          >
+            Google SignIn
+          </Button>
+        </ButtonsContainer>
+      </form>
+    </SignInContainer>
+  );
+};
 
 export default SignInForm;
